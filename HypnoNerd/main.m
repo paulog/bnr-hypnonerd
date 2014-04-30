40a2b90f9e815f898d51d9fb5afda4870493c39a
Paulo Gonzaga <paulo.gonzaga@gmail.com>
2014-04-29 22:02:09 -0400

    Initial Commit

---
+++ HypnoNerd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by Paulo Gonzaga on 4/29/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
